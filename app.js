24f6ebb387a796c9a6e68c34fbfd669f0bb24732
HariKodapaka@MBP <chaitanyakodapaka@gmailcom>
2019-09-04 21:37:54 -0400

    CORS added

--- app.js
+++ app.js
@@ -15,6 +15,22 @@ var auth = require('./routes/auth.routes');
 // Init App
 var app = express();
 
+var allowedOrigins = ['http://localhost:4200',
+                      'https://amfusion.netlify.com/'];
+app.use(cors({
+  origin: function(origin, callback){
+    // allow requests with no origin 
+    // (like mobile apps or curl requests)
+    if(!origin) return callback(null, true);
+    if(allowedOrigins.indexOf(origin) === -1){
+      var msg = 'The CORS policy for this site does not ' +
+                'allow access from the specified Origin.';
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  }
+}));
+
 app.use(expressJwt({
     secret: config.secret,
     getToken: function (req) {
